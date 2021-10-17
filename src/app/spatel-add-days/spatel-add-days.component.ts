a3f21636bbe5ab9e82571a35083f0aac6af0c6cf
sp323 <patel.sujal.r@gmail.com>
2021-10-17 15:52:08 -0500

    Adding in materials attributes

--- src/app/spatel-add-days/spatel-add-days.component.ts
+++ src/app/spatel-add-days/spatel-add-days.component.ts
@@ -20,5 +20,28 @@ export class SpatelAddDaysComponent implements OnInit {
   get todaysDate() {
       return (new Date());
   }
+  inputDays(){
+      const checkP = document.getElementById("newDateP");
+      if(checkP){
+          checkP.remove();
+      }
+      const newDate = new Date();
+      const input = (<HTMLInputElement>document.getElementById("inputDays")).value;
+      console.log(input);
+      newDate.setDate(newDate.getDate() + parseInt(input));
+      console.log(newDate);
+
+
+      const months = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+
+      const newP = document.createElement("h2");
+      newP.setAttribute("id", "newDateP");
+      newP.setAttribute("style" , "margin-left:10%");
+      newP.innerHTML = ("The New Date is " + newDate.getFullYear() + "-" + months[newDate.getUTCMonth()] + '-' + newDate.getDate());
+      document.body.appendChild(newP);
+
+
+
+  }
 
 }
