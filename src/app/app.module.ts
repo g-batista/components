61fd18e1a0cd40e382d029fdfe3662d66cca11a0
g-batista <gbatista@madisoncollege.edu>
2021-11-06 11:10:28 -0500

    Starting to build the addition component
    Remember to import mat controls I plan ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,11 +15,13 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatSelectModule } from '@angular/material/select';
 import { JvSlackNTellComponent } from './jv-slack-n-tell/jv-slack-n-tell.component';
 
+
 import { JacComponentComponent } from './jac-component/jac-component.component';
 import { MatFormFieldModule} from '@angular/material/form-field';
 
 import { TjsMultiplicationComponent } from './tjs-multiplication/tjs-multiplication.component';
 import { MatInputModule } from '@angular/material/input';
+
 import { MtbSelectComponent } from './mtb-select/mtb-select.component';
 import { CommonModule } from '@angular/common';
 import { GbatistaAddComponent } from './gbatista-add/gbatista-add.component';
